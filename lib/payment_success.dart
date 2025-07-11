2893bc25d7abca628e6711c2683528cd68030de0
Stephanie <stephanie@students.mikroskil.ac.id>
2025-07-11 18:12:51 +0700

    payment_success

--- lib/payment_success.dart
+++ lib/payment_success.dart
@@ -2,8 +2,6 @@ import 'package:eclipse_code/navigator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-
-
 class PaymentSuccess extends StatelessWidget {
   final bool showSnackbar;
 
@@ -12,6 +10,7 @@ class PaymentSuccess extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
 
     if (showSnackbar) {
       WidgetsBinding.instance.addPostFrameCallback((_) {
@@ -27,58 +26,75 @@ class PaymentSuccess extends StatelessWidget {
     }
 
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SvgPicture.asset(
-              '8.svg',
-              width: screenWidth*1,
-              height: screenWidth*1,
-              fit: BoxFit.contain,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.08),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: screenHeight * 0.08), // Spacer atas
+                SvgPicture.asset(
+                  '8.svg',
+                  width: screenWidth * 0.7,
+                  height: screenWidth * 0.7,
+                  fit: BoxFit.contain,
+                ),
+                SizedBox(height: screenWidth * 0.08),
+                Text(
+                  'Thank You!',
+                  style: TextStyle(
+                    color: Color(0xFF7B1113),
+                    fontWeight: FontWeight.bold,
+                    fontSize: screenWidth * 0.1,
+                  ),
+                ),
+                Text(
+                  'Payment done Successfully',
+                  style: TextStyle(
+                    color: Color(0xFF7B1113),
+                    fontSize: screenWidth * 0.04,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: screenWidth * 0.09),
+                Text(
+                  'You will be redirected to Home Page by clicking continue',
+                  style: TextStyle(
+                    color: Color(0xFF7B1113),
+                    fontSize: screenWidth * 0.04,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: screenWidth * 0.1),
+              ],
             ),
-            Text('Thank You!', 
-            style: TextStyle(
-              color: Color(0xFF7B1113), 
-              fontWeight: FontWeight.bold,
-              fontSize: screenWidth*0.1
-              ),
-            ),
-            Text('Payment done Successfully', 
-            style: TextStyle(
-              color: Color(0xFF7B1113),
-              fontSize: screenWidth*0.04,
-              fontWeight: FontWeight.bold
-              ),
-            ),
-            SizedBox(height: screenWidth*0.09),
-            Text('You will be redirected to Home Page by clicking continue', 
-            style: TextStyle(
-              color: Color(0xFF7B1113),
-              fontSize: screenWidth*0.04,
-              fontWeight: FontWeight.bold
-              ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: screenWidth*0.1),
-          ],
+          ),
         ),
-        
       ),
       bottomNavigationBar: SizedBox(
         width: screenWidth,
-        height: screenWidth*0.15,
+        height: screenWidth * 0.15,
         child: ElevatedButton(
           style: ElevatedButton.styleFrom(
-          backgroundColor: Color(0xFF7B1113),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero), 
-        ),
-          onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context) => Nav()));},
-          child: Text('Continue', style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: screenWidth*0.05
-          ),),
+            backgroundColor: Color(0xFF7B1113),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+          ),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Nav()),
+            );
+          },
+          child: Text(
+            'Continue',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: screenWidth * 0.05,
+            ),
+          ),
         ),
       ),
     );
